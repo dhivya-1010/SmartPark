a9687f097e38548e19f7995819c0c59067e59133
dhivya-1010 <dhivya.v2024cse@sece.ac.in>
2026-07-10 13:13:11 +0530

    filtering columns

--- data/columnVerification.py
+++ data/columnVerification.py
@@ -5,4 +5,14 @@ df = pd.read_csv(
     low_memory=False
 )
 
-print(df["local_authority_name"].unique())
+birmingham = df[df["local_authority_name"] == "Birmingham"]
+
+print("Number of Birmingham records:", len(birmingham))
+
+print(birmingham[[
+    "road_name",
+    "latitude",
+    "longitude",
+    "all_motor_vehicles",
+    "year"
+]].head())
